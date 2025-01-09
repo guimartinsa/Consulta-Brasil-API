7081de4176bff4c01129c0df21dd9f9962f50240
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-01-09 06:26:04 -0300

    Mensagem do commit

--- api.py
+++ api.py
@@ -26,7 +26,7 @@ def consultar_cnpj(cnpj):
         return {"erro": f"Erro na consulta do CNPJ: {e}"}
     except Exception as e:
         return {"erro": f"Erro inesperado: {e}"}
-    
+
 # Função para consultar feriados
 def consultar_feriado(ano):
     url = f"https://brasilapi.com.br/api/feriados/v1/{ano}"
@@ -48,22 +48,28 @@ def index():
 @app.route("/consulta/cep", methods=["POST"])
 def consulta_cep():
     cep = request.form.get("cep")
+    if not cep:
+        return render_template("index.html", cep_result={"erro": "CEP não informado"})
     resultado = consultar_cep(cep)
-    return jsonify(resultado)
+    return render_template("index.html", cep_result=resultado)
 
 # Rota para consulta de CNPJ
 @app.route("/consulta/cnpj", methods=["POST"])
 def consulta_cnpj():
     cnpj = request.form.get("cnpj")
+    if not cnpj:
+        return render_template("index.html", cnpj_result={"erro": "CNPJ não informado"})
     resultado = consultar_cnpj(cnpj)
-    return jsonify(resultado)
+    return render_template("index.html", cnpj_result=resultado)
 
 # Rota para consulta de feriados
 @app.route("/consulta/feriados", methods=["POST"])
 def consulta_feriados():
     ano = request.form.get("ano")
+    if not ano:
+        return render_template("index.html", feriado_result={"erro": "Ano não informado"})
     resultado = consultar_feriado(ano)
-    return jsonify(resultado)
+    return render_template("index.html", feriado_result=resultado)
 
 if __name__ == "__main__":
     app.run(debug=True)
