741c6145a801eb2cd1481cc3c319bb5663a08480
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-01-13 00:21:01 -0300

    correção1 js

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Função para consultar Bancos
 def consultar_banco(code):
